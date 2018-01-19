c19ef861c352ff384a1e773c5f2e7b61c073d313
Lisa Caton <lisacaton01@gmail.com>
2018-01-19 12:09:35 -0500

    Finished with Part 3: Player Bar Buttons

--- scripts/player.js
+++ scripts/player.js
@@ -13,14 +13,15 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+// default parameter value only valid if no value is passed in when function is called
   playPause (song = this.currentlyPlaying) {
+    //if currentlyPlaying is not the same as song
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
@@ -37,12 +38,12 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +51,3 @@ class Player {
 }
 
 const player = new Player();
-
